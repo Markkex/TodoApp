9eb5dd673c03985d7df6afd390a9ec7e2d375d70
Markkex <marco.avila.93@hotmail.com>
2021-06-06 23:32:42 +0100

    inputTextHandler and SubmitTodoHandler

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Form from "./Components/Form";
+import TodoList from "./Components/TodoList";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+
   return (
-    <div className="App">
-     <h1>Hello React</h1>
+    <div className='App'>
+      <header>
+        <h1>Markkex's Todo List</h1>
+      </header>
+      <Form setInputText={setInputText} todos={todos} setTodos={setTodos} inputText={inputText}/>
+      <TodoList inputText={inputText} />
     </div>
   );
 }
